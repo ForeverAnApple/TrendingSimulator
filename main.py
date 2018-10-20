27847be8dda4b73ad45642d5ba4103de008a254e
mburnes <mtburnes@gmail.com>
2018-10-20 02:07:51 -0500

    Personal main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import markovify
+
+
 def main():
     print('Welcome to TrendyTwitter Bot!')
+    with open("./lorem.txt") as file:
+        string = file.read()
+        print(string)
+
+    text_model = markovify.Text(string)
+
+    print(text_model.make_sentence())
+    for i in range(3):
+        print(text_model.make_short_sentence(140))
 
 
 if __name__ == '__main__':
