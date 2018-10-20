91284f232e48b49dded72ae50e865aeff3d94dbc
Daiwei Chen <kinbd8@gmail.com>
2018-10-20 12:10:30 -0500

    Google Cloud Vision API up and running for one image, returns top labels for the requested image.

---
+++ cloud-vision.py
@@ -0,0 +1,54 @@
+import requests
+# from google.cloud.vision import types
+
+class VisionApi():
+    def __init__(self, img_url):
+        # [START vision_quickstart]
+
+        # Instantiates a client
+        # [START vision_python_migration_client]
+
+        api_token = 'AIzaSyB_qqDMXqKi1-mXtXj-zwtCVqb2TG2eAtk'
+        self.img_url = img_url
+        self.request = {}
+        self.DISCOVERY_URL = 'https://vision.googleapis.com/v1/images:annotate?key=' + api_token
+
+    def getLabels(self):
+        self.request = {
+            "requests": [
+                {
+                    "image": {
+                        "source": {
+                            "imageUri":
+                                self.img_url
+                        }
+                    },
+                    "features": [
+                        {
+                            "type": "LABEL_DETECTION",
+                            "maxResults": 3
+                        }
+                    ]
+                }
+            ]
+        }
+
+        response = requests.post(self.DISCOVERY_URL, json=self.request)
+        print(response.status_code, response.reason)
+        jres = response.json()
+        labels = [annotations['description'] for annotations in jres['responses'][0]['labelAnnotations']]
+        print("Descriptions for " + self.img_url + ": " + labels)
+        # labels = response.label_annotations
+        # print('Labels for ' + self.img_url + ':')
+        # for label in labels:
+        #     print(label.description)
+        return labels
+
+
+def main():
+    test_img = VisionApi('https://i.imgur.com/BBcy6Wc.jpg')
+    test_img.getLabels()
+
+
+if __name__ == '__main__':
+    main()
